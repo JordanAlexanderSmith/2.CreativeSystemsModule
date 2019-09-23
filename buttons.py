3ff7441f37ba34fd473d55601eb45011f6e429a9
Jordan Smith <jdudeisme3000@gmail.com>
2019-09-23 01:07:55 -0400

    Update buttons.py
    Having connected all three components to the Pi

--- buttons.py
+++ buttons.py
@@ -1,17 +1,24 @@
 
 
+
 #GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP) //pin n   
+
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+# GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 x = 0
+y = 0
 
 
 GPIO.add_event_detect(21, GPIO.RISING)  # add rising edge detection on a channel
-GPIO.add_event_detect(20, GPIO.RISING)
+GPIO.add_event_detect(20, GPIO.RISING) # switc
+GPIO.add_event_detect(23, GPIO.RISING) # stick X
+# GPIO.add_event_detect(18, GPIO.RISING) 
 
 while x < 5:
 	if GPIO.event_detected(21):
@@ -22,11 +29,19 @@ while x < 5:
 		print('switched')
 		print(x)
 		break
+	if GPIO.event_detected(23):
+		print('Stick Down')
+		y+=1
+		print(y)
+	# if GPIO.event_detected(18):
+	# 	print('OTHER Stick')
+	# 	y-=1
+	# 	print(y)
+
 
 print GPIO.input(21)
 print GPIO.input(20)
-
-
+print GPIO.input(23)
 
 
 
