916f9e4be21faab82125302f5091ac3fceef7ba1
Jordan Smith <jdudeisme3000@gmail.com>
2019-09-23 00:37:05 -0400

    Update buttons.py
    Added switch break

--- buttons.py
+++ buttons.py
@@ -2,24 +2,33 @@
 
 #GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP) //pin n   
 
-
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 x = 0
 
 
 GPIO.add_event_detect(21, GPIO.RISING)  # add rising edge detection on a channel
+GPIO.add_event_detect(20, GPIO.RISING)
 
 while x < 5:
 	if GPIO.event_detected(21):
 	    print('Button pressed')
 	    x+=1
 	    print(x)
-
+	if GPIO.event_detected(20):
+		print('switched')
+		print(x)
+		break
 
 print GPIO.input(21)
+print GPIO.input(20)
+
+
+
+
 
 
 
